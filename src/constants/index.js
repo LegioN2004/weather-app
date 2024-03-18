92c7b9da8b9e6660cab2e721350044404ee5847c
LegioN2004 <milindabarua04@gmail.com>
2024-03-18 12:55:23 +0530

    first app commit, more changes to be made

---
+++ src/constants/index.js
@@ -0,0 +1,20 @@
+export const apiKey = 'f920addd8a3443abbe5120128240803';
+
+export const weatherImages = {
+  'Clear': require('../assets/sun.png'), // 'Clear' is the weather condition for 'Sunny
+  'Sunny': require('../assets/sun.png'),
+  'Overcast': require('../assets/cloud.png'),
+  'Cloudy': require('../assets/cloud.png'),
+  'Partly cloudy': require('../assets/partly-cloudy.png'),
+  'Mist': require('../assets/partly-cloudy.png'),
+  'Moderate rain': require('../assets/moderate-rain.png'),
+  'Patchy rain possible': require('../assets/moderate-rain.png'),
+  'Light rain shower': require('../assets/moderate-rain.png'),
+  'Moderate rain at times': require('../assets/moderate-rain.png'),
+  'Heavy rain': require('../assets/heavy-rain.png'),
+  'Heavy rain at times': require('../assets/heavy-rain.png'),
+  'Moderate or heavy freezing rain': require('../assets/heavy-rain.png'),
+  'Moderate or heavy rain shower': require('../assets/heavy-rain.png'),
+  'Moderate or heavy rain with thunder': require('../assets/heavy-rain.png'),
+  'other ': require('../assets/sun.png'),
+};
